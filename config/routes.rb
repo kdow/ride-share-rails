a2ad3462c5a176e798236c642287c92c9337c8e5
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-15 16:18:56 -0700

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :passengers
+  resources :drivers
+  resources :trips, except: [:index, :new]
 end
