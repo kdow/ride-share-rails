a603a5094704bc7129d0aea005813d48256164db
Kelly Downes <kedownes@gmail.com>
2019-04-15 16:45:40 -0700

    Add driver controller and methods

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -3,16 +3,41 @@ require "test_helper"
 describe DriversController do
   describe "index" do
     it "can get index" do
-      # Your code here
+      get drivers_path
+
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get a valid driver" do
+      get driver_path(driver.id)
+
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid driver" do
+      get driver_path(-1)
+
+      must_respond_with :redirect
+    end
   end
 
   describe "edit" do
-    # Your tests go here
+    it "can get the edit page for an existing driver" do
+      get edit_driver_path(driver.id)
+
+      must_respond_with :success
+    end
+
+    it "will respond with redirect when attempting to edit a nonexistant driver" do
+      # Act
+      get edit_driver_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to drivers_path
+    end
   end
 
   describe "update" do
@@ -20,7 +45,11 @@ describe DriversController do
   end
 
   describe "new" do
-    # Your tests go here
+    it "can get the new driver page" do
+      get new_driver_path
+
+      must_respond_with :success
+    end
   end
 
   describe "create" do
