bcf4ddb9a2793b440af4732f66ab9e336b0ad471
Kelly Downes <kedownes@gmail.com>
2019-04-16 15:59:17 -0700

    Add validation to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  validates :date, presence: true
+  validates :cost, presence: true
+  validates :passenger_id, presence: true
+  validates :driver_id, presence: true
 end
