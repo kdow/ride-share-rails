cea6651ff47bdb7ed565904ae93021e44f630bc2
Kelly Downes <kedownes@gmail.com>
2019-04-17 12:30:18 -0700

    Add trip index controller method and view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,16 @@
 class TripsController < ApplicationController
+  def index
+    if params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trips = passenger.trips
+    elsif params[:driver_id]
+      driver = Driver.find_by(id: params[:driver_id])
+      @trips = driver.trips
+    else
+      @trips = Trip.all
+    end
+  end
+
   def create
     trip = Trip.new(trip_params)
 
