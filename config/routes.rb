21a3f74949bba8dfc1d04003c084f98412587eeb
Kelly Downes <kedownes@gmail.com>
2019-04-17 12:12:07 -0700

    Update routes to include nested passengers andd drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root "homepages#index"
-  
+
+  resources :passengers do
+    resources :trips, only: [:index, :create]
+  end
+
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
+
   resources :passengers
   resources :drivers
-  resources :trips, except: [:index, :new]
+  resources :trips, except: [:new, :create]
 end
