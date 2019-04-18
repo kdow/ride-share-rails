2e59715684075bdd5d22d86d925298903bd9dcda
Kelly Downes <kedownes@gmail.com>
2019-04-18 09:43:12 -0700

    Create model method for driver total earnings and add into view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,12 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_earnings
+    earnings = 0
+    self.trips.each do |trip|
+      earnings += ((trip.cost - 165) * 0.8)
+    end
+    return earnings
+  end
 end
