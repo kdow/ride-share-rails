977ab707d41bfa4e57f63f1354f2aa044e9024f1
Kelly Downes <kedownes@gmail.com>
2019-04-18 21:25:23 -0700

    Add driver avg rating method and show view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -7,4 +7,20 @@ class Driver < ApplicationRecord
   def total_earnings
     return self.trips.sum { |trip| (trip.cost - 165) * 0.8 }
   end
+
+  def average_rating
+    total = 0
+    count = 0
+    self.trips.each do |trip|
+      if trip.rating
+        total += trip.rating
+        count += 1
+      end
+    end
+    if count == 0
+      return "This driver has no ratings"
+    else
+      return total / count
+    end
+  end
 end
